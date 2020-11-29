1ebe220f1e42bceede2e80430a9323af54061437
vehitro <s.ramirezmonte@alumnos.duoc.cl>
2020-11-29 19:29:15 -0300

    vehitro

---
+++ blog/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Auto(models.Model):
+    modelo = models.CharField(max_length=200, default='', blank=False)
+    anio = models.IntegerField()
+    color = models.CharField(max_length=200, default='')
+    stock = models.BooleanField(default=False)
+    marca = models.CharField(max_length=200, default='')
+    imagen = models.ImageField(upload_to='images/', default='')
+
+    def __str__(self):
+        return self.modelo
